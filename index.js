da3d38ff45f4eb9a7f2beece9867a7cc9802cb3c
oxyum services <29GariglioSe@fortcherry.org>
2025-02-28 14:41:16 -0500

    push

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ const io = require('socket.io')(http);
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + 'public', 'client.html');
+  res.sendFile(__dirname + 'public', 'index.html');
 });
 
 app.get('/viewer', (req, res) => {
-    res.sendFile(__dirname + 'public', 'index.html');
+    res.sendFile(__dirname + 'public', 'client.html');
 });
 
 io.on('connection', (socket) => {

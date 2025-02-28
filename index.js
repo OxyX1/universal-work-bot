b0ebe4acdef200024d2bd9b89d3721c12f939b51
oxyum services <29GariglioSe@fortcherry.org>
2025-02-28 14:21:00 -0500

    push

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+  console.log('A user connected');
+
+  socket.on('screen-share', (stream) => {
+    io.emit('screen-share', stream);
+  });
+
+  socket.on('stop-sharing', () => {
+    io.emit('stop-sharing');
+  });
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected');
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+http.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});

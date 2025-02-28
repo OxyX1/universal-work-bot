64f9c159b875b243adbb2dcf6cf2ace4fb5a1c7e
oxyum services <29GariglioSe@fortcherry.org>
2025-02-28 15:58:52 -0500

    push

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const { fork } = require('child_process');
 const cors = require('cors');
+const { v4: uuidv4 } = require('uuid');
 const app = express();
 const port = 8080;
 
 app.use(cors()); // Add this line to enable CORS
+
+const rooms = {};
+
+app.use(express.json()); // For parsing JSON requests
 
 app.get('/random64', (req, res) => {
     const child = fork('api/random64.js');
@@ -13,6 +18,35 @@ app.get('/random64', (req, res) => {
         res.send(message);
     });
     child.send('start');
+});
+
+// Create a new room
+app.post('/create-room', (req, res) => {
+    const roomId = uuidv4();
+    rooms[roomId] = {}; // Initialize room with empty object
+    res.send({ roomId });
+});
+
+// Add data to room
+app.post('/room/:roomId/add', (req, res) => {
+    const { roomId } = req.params;
+    const { key, value } = req.body;
+    if (rooms[roomId]) {
+        rooms[roomId][key] = value;
+        res.send({ message: 'Data added to room' });
+    } else {
+        res.status(404).send({ message: 'Room not found' });
+    }
+});
+
+// Retrieve data from room
+app.get('/room/:roomId', (req, res) => {
+    const { roomId } = req.params;
+    if (rooms[roomId]) {
+        res.send(rooms[roomId]);
+    } else {
+        res.status(404).send({ message: 'Room not found' });
+    }
 });
 
 app.use((req, res, next) => {

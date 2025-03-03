a1d846a653f21dadd76ffe42297e36ba43c5bbbc
oxyum services <29GariglioSe@fortcherry.org>
2025-03-02 21:58:53 -0500

    push

--- api/vcs.js
+++ api/vcs.js
@@ -1,15 +1,25 @@
-const { exec } = require('child_process');
+const { spawn } = require('child_process');
+const { v4: uuidv4 } = require('uuid');
 
-process.on('message', (cmd) => {
-    exec(cmd, (error, stdout, stderr) => {
-        if (error) {
-            process.send({ error: error.message });
-            return;
-        }
-        if (stderr) {
-            process.send({ error: stderr });
-            return;
-        }
-        process.send({ output: stdout });
+const sessions = {}; // Store user shell processes
+
+process.on('message', (message) => {
+    const { sessionId, command } = message;
+
+    if (!sessions[sessionId]) {
+        // Start a new shell process for this session
+        sessions[sessionId] = spawn('bash'); // Change to 'cmd' for Windows
+    }
+
+    const shell = sessions[sessionId];
+
+    shell.stdout.once('data', (data) => {
+        process.send({ sessionId, output: data.toString() });
     });
+
+    shell.stderr.once('data', (data) => {
+        process.send({ sessionId, error: data.toString() });
+    });
+
+    shell.stdin.write(command + '\n'); // Execute command
 });
